7d2075f8743985c9d204735802a12a0f7c78c309
Sophia Alvarez <101871038+SAlvarezBulfon@users.noreply.github.com>
2022-06-29 15:39:06 -0300

    Se agregaron las verificaciones

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,13 +1,78 @@
 import csv
+import sys
+import os 
+
+
+# PASO 2 !
+if len(sys.argv) < 5:
+    print('Hay que ingresar por lo menos 4 parámetros') 
+    exit()
+if len(sys.argv) > 8:
+    print('Hay parámetros de más')
+    exit()
+
+archivo = sys.argv[1]
+dni = sys.argv[2]
+salida = sys.argv[3]
+tipo = sys.argv[4]
+estado = sys.argv[5] if len(sys.argv) > 5 else None
+fecha = sys.argv[6] if len(sys.argv) > 6 else None
+
+
+# PASO 3 !!
+# PARA VERIFICAR SI EL ARCHIVO EXISTE
+if not os.path.exists(archivo) or not os.path.isfile(archivo):
+    print('El archivo no existe')
+    exit()
+
+try:
+    dni = int(dni)
+except ValueError:
+    print('No es un dni válido')
+    exit()
+
+if salida not in ['PANTALLA','CSV']:
+    print('No es un parámetro de salida válido')
+    exit()
+
+if tipo not in ['EMITIDO','DEPOSITADO']:
+    print('No es un parámetro de tipo válido')
+    exit()
+
+if estado and estado not in ['PENDIENTE','APROBADO', 'RECHAZADO']:
+    print('No es un parámetro de tipo válido')
+
+if fecha:
+    if len(fecha.split(':')) != 2:
+        print('No es una fecha correcta')
+        exit()
+    desde,hasta = fecha.split(':')
+    if len(desde) != 10 or len(desde.split('-')) != 3:
+        print('Fecha desde no es correcta')
+        exit()
+    if len(hasta) != 10 or len(hasta.split('-')) != 3:
+        print('Fecha hasta no es correcta')
+        exit()
+    ddia,dmes,danho = desde.split('-')
+    hdia,hmes,hanho = hasta.split('-')
+
+    if len(ddia) != 2 or len(dmes) != 2 or len(danho) !=4:
+        print('Fecha desde no es correcta')
+        exit()
+    
+    if len(hdia) != 2 or len(hmes) != 2 or len(hanho) !=4:
+        print('Fecha hasta no es correcta')
+        exit()
+    
+
 
 def get_cheques(file):
     with open(file, 'r') as archivo:
         cheques = archivo.read().splitlines()
-        print(cheques)
         cheques.pop(0)
         chequesConFormato = []
         for l in cheques:
-            cheque = l.split(';')
+            cheque = l.split(',')
             chequesConFormato.append(
                 {
                     "NroCheque" : cheque[0],
@@ -19,7 +84,8 @@ def get_cheques(file):
                     "FechaOrigen" : cheque[6],
                     "FechaPago" : cheque[7],
                     "DNI" : cheque[8],
-                    "Estado" : cheque[9]
+                    "Tipo": cheque[9],
+                    "Estado" : cheque[10]
                 })
             
         return chequesConFormato
@@ -57,10 +123,10 @@ def export_csv(cheques):
 
 def main(csv):
     cheques = get_cheques(csv)
-    # print_cheques(cheques)
-    export_csv(cheques)
+    print_cheques(cheques)
+    #export_csv(cheques)
 
-main('cheques.csv')
+main(archivo)
 
 '''
 -------------------
